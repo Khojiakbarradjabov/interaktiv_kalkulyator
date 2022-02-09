62f1b80c121572bcf4c867d7345cc6de848c3393
Khojiakbarradjabov <99033548+Khojiakbarradjabov@users.noreply.github.com>
2022-02-09 14:40:59 +0500

    Add files via upload
    gthjkl

---
+++ sodda_kalkulyator.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 00:50:40 2022
+
+@author: khoji
+"""
+
+#Интерактив калкулятор
+
+
+print("Solishtirish kalkulyatorini ishlatish uchun shartlarini bajaring")
+a = float(input("Birinchi raqam: ")) # sonni so'z sifatida kiritgani uchun avvalo o'nlik songa aylantirib olishimis kerak. Shu sababli float() ishlatamiz
+what = input("Qanday amal bajarish kerak? (*,/,+,-): ")
+
+b = float(input("Ikkinchi raqam: "))
+d = float(input("Solishtirish uchun javobingizni kiriting: "))
+if what == "+":
+    c = a+b
+    if d==c:
+        print("To'ppa to'gri")
+    elif d>=c:
+        print("Ko'payib ketti")
+    elif d<=c:
+        print("Kam hisobladingiz")
+elif what == "-":
+    c = a-b
+    if d==c:
+        print("To'ppa to'gri")
+    elif d>=c:
+        print("Ko'payib ketti")
+    elif d<=c:
+        print("Kam hisobladingiz")
+elif what == "*":
+    c = a*b
+    if d==c:
+        print("To'ppa to'gri")
+    elif d>=c:
+        print("Ko'payib ketti")
+    elif d<=c:
+        print("Kam hisobladingiz")
+elif what == "/":
+    c = a/b
+    if d==c:
+        print("To'ppa to'gri")
+    elif d>=c:
+        print("Ko'payib ketti")
+    elif d<=c:
+        print("Kam hisobladingiz")
+else:
+    print("Hato operaciya") 
